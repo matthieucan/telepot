8ca06506b854f23ee4ed245637790d1a8b72cd45
Matthieu Caneill <matthieu.caneill@imag.fr>
2016-03-16 22:28:53 +0100

    add basic command line features

--- setup.py
+++ setup.py
@@ -82,6 +82,9 @@ setup(
     # Choose your license
     license='MIT',
 
+    # Scripts
+    scripts = ['bin/telepot-do'],
+
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
         # How mature is this project? Common values are
